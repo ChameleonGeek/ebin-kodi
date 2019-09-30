ea31c7fee35ae5d1745c4ba89437bc629b60aac8
ChameleonGeek <55959244+ChameleonGeek@users.noreply.github.com>
2019-09-29 23:38:02 -0700

    Update ebin-sd-pi.sh

--- ebin-sd-pi.sh
+++ ebin-sd-pi.sh
@@ -33,19 +33,19 @@ if [ -e 'ebin-ubuntu-16.04.3.zip' ]; then
 	DLSTATUS=1
 fi
 
-if [ $DLSTATUS == 0 && $EXTRACTED == 0 ]; then
+if [ $DLSTATUS = 0 ] && [ $EXTRACTED = 0 ]; then
 	# IMAGE HAS NOT BEEN DOWNLOADED OR EXTRACTED
 	wget http://espressobin.net/wp-content/uploads/2017/10/ebin-ubuntu-16.04.3.zip
 fi
 
-if [ $EXTRACTED == 0 ]; then
+if [ $EXTRACTED = 0 ]; then
 	# IMAGE HAS BEEN DOWNLOADED BUT NOT EXTRACTED
 	unzip ebin-ubuntu-16.04.3.zip	
 fi
 
-if [ -e 'rootfs.tar.bz2' ]; then
+if ! [ -e 'rootfs.tar.bz2' ]; then
 	echo "The EspressoBin file system has not been extracted."
-  exit
+	exit
 fi
 
 # OS IMAGE HAS BEEN DOWNLOADED AND UNZIPPED.  NOW PREPARE THE SD CARD
@@ -53,20 +53,21 @@ MOUNTPOINT=$(whiptail --title "Mount Point" --inputbox "Enter the current mount 
 esa=$?
 DEVICEPATH=$(whiptail --title "Device Path" --inputbox "Enter the device path for the SD card" 8 78 "/dev/sda1" 3>&1 1>&2 2>&3)
 esb=$?
-if [ ((esa + esb)) == 0 ]; then
-  sudo umount "${MOUNTPOINT}"
-  sudo mkdir /ebincard
-  sudo mkfs -t ext4 "${DEVICEPATH}"
-  sudo mount "${DEVICEPATH}" /ebincard
-  cd /ebincard
-  sudo tar -xvf /home/pi/rootfs.tar.bz2
-  cd /home/pi
-  sudo umount /ebincard
-  sudo rm -rf /ebincard
-  echo "Card has been prepped and is safe to disconnect."
+
+if [ $esa = 0 ] && [ $esb = 0 ]; then
+	sudo umount "${MOUNTPOINT}"
+	sudo mkdir /ebincard
+	sudo mkfs -t ext4 "${DEVICEPATH}"
+	sudo mount "${DEVICEPATH}" /ebincard
+	cd /ebincard
+	sudo tar -xvf /home/pi/rootfs.tar.bz2
+	cd /home/pi
+	sudo umount /ebincard
+	sudo rm -rf /ebincard
+	echo "Card has been prepped and is safe to disconnect."
 else
-  echo "Card prep failed."
-  if ![ -e 'ebin-ubuntu-16.04.3.zip' ]; then
-	  echo "The image zip file has not been downloaded."
-  fi
+	echo "Card prep failed."
+	if ![ -e 'ebin-ubuntu-16.04.3.zip' ]; then
+		echo "The image zip file has not been downloaded."
+	fi
 fi
